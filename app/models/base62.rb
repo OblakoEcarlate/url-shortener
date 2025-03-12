872f672a7ea9285806061c7c5f56520402c8128a
OblakoEcarlate <oblako.soleil@gmail.com>
2025-03-12 11:04:33 +0100

    Add encoding and decoding with base62

---
+++ app/models/base62.rb
@@ -0,0 +1,30 @@
+class Base62
+  ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
+  BASE = ALPHABET.length
+  def self.encode(number)
+    return ALPHABET.first if number.zero? || number.nil?
+
+    result = ""
+
+    while number > 0 do
+      index = number % BASE
+      char = ALPHABET[index]
+      result.prepend char
+      number /= BASE
+    end
+
+    result
+  end
+
+  def self.decode(string)
+    number = 0
+
+    string.reverse;each_char.with_index do |char, index|
+      power = BASE**index
+      index = ALPHABET.index(char)
+      number += index * power
+    end
+
+    number
+  end
+end
